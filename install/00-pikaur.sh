e7fc681769a28e4aace7c6c555824eb93c282842
GraeWolf <kmccuddy@protonmail.com>
2025-07-19 19:42:24 -0400

    separated the install for pikaur and brave

--- install/00-pikaur.sh
+++ install/00-pikaur.sh
@@ -1,12 +1,22 @@
-#!/usr/bin/env bash
-
 tput setaf 7;echo "################################################################"
-echo "Installing Brave Browser"
+echo "Installing Aur Helper Pikaur"
 echo "################################################################"
 echo;tput sgr0
-pikaur -S --noconfirm --needed brave-bin
+
+sudo pacman -S --noconfirm --needed base-devel
+
+if ! command -v pikaur &>/dev/null; then
+    cd /tmp
+    git clone https://aur.archlinux.org/pikaur.git
+    cd pikaur
+    makepkg -fsri
+    cd -
+    rm -rf pikaur
+    cd ~
+fi
 
 tput setaf 7;echo "################################################################"
-echo "Installation of Brave complete"
+echo "Installation of Pikaur complete"
 echo "################################################################"
 echo;tput sgr0
+
